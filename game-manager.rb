0b7c3bd3e98d3222964fe1891112789397bdf447
Stefan Talbot <satalbot@protonmail.com>
2022-12-12 17:12:10 -0500

    Refactor
    # Refactored: Method names, and inputs. Extracted some functionality into helper methods.
    + Added: Comments to code.

--- game-manager.rb
+++ game-manager.rb
@@ -5,27 +5,42 @@ class GameManager
   attr_accessor :players, :current_player, :current_sate, :winner
   STATES = {active: "active", game_over: "game_over"}
   
-  def initialize(one, two)
+  def initialize(player_names)
+    # Create players
     @players = []
-    player_name = one
-    while @players.length < 2 do
-      @players.push(Player.new(player_name))
-      player_name = two
-    end
+    create_players(player_names)
+
+    # Set other attributes
     @current_player = @players[0]
     @current_sate = STATES[:active]
-    state_manager()
+
+    # Start game loop
+    game_manager()
   end
 
-  def state_manager()
+  # Creates player objects
+  private def create_players(names)
+    @players.push(Player.new(names[0]))
+    @players.push(Player.new(names[1]))
+  end
+
+  # Main game loop
+  private def game_manager()
     puts "-- [ Game Start! ] --"
+
+    # Game loop
     while(@current_sate != STATES[:game_over])
       puts ""
       puts "NEW ROUND"
+
+      # Create new question
       question = Question.new
       puts "#{@current_player.name}: #{question.question}"
+
+      # Get players answer
       answer = gets.chomp.to_i
       
+      # Check players answer
       if(question.check_answer(answer))
         puts "#{@current_player.name} was CORRECT!"
       else
@@ -33,15 +48,28 @@ class GameManager
         @current_player.lives -= 1
       end
 
+      # Display score
       puts "Lives: #{@players[0].name} = #{@players[0].lives}/3 VS #{@players[1].name} = #{@players[1].lives}/3"
-      if(@current_player.lives <= 0)
-        @current_sate = STATES[:game_over]
-        @winner = @players.select {|player| player != @current_player}[0]
-      end
 
-      @current_player = @players.select {|player| player != @current_player}[0]
+      # Check for game over
+      if(@current_player.lives <= 0)
+        game_over()
+      else
+        @current_player = get_other_player() 
+      end
     end
+  end
+
+  private def game_over()
+    @current_sate = STATES[:game_over]
+    @winner = get_other_player()
     puts ""
+    puts "-- [ Game Over! ] --"
     puts "Winner is #{@winner.name}!"
+  end
+
+  # Returns the non-current player
+  private def get_other_player()
+    @players.select {|player| player != @current_player}[0]
   end
 end
